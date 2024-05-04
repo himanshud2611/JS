d37506d0fd9c372ce59ef08ca77616dfd5085a86
Himanshu Dubey <101963392+himanshud2611@users.noreply.github.com>
2024-05-04 17:29:26 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 28200
+let accountEmail = "hdubey261102@gmail.com"
+var accountPassword = "12543"
+accountCity = "Gorakhpur"
+let accoutnState
+
+//accountId = 22407 [Not Allowed]
+
+/*
+prefer not to use "var" because
+of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accoutnState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+// number => 2 power 53
+// bigint
+// string => " "
+// boolean => true OR false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+console.log(typeof "")
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
