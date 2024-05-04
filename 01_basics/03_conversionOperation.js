9a24ea84b3caf6b9b4099a33f41858779a58d0db
Himanshu Dubey <101963392+himanshud2611@users.noreply.github.com>
2024-05-04 19:21:15 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = null
+
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+/*"33" => 33
+"33abc" => NaN
+true => 1, false => 0
+*/
+
+let isBoolean = ""
+let BooleanIsLoggedIn = Boolean(isBoolean)
+console.log(BooleanIsLoggedIn);
+
+/*
+1 => true, 0 => false
+""=> false, "JS" => true
+*/
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber); //STRING
+
+
+
